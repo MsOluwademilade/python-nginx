022509f4bf3f38205eb5eace021f3f273c8239f0
MsOluwademilade <oyekanmidemilade2@gmail.com>
2024-12-31 12:26:16 +0000

    Edited python and docker file

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,5 @@ COPY requirements.txt .
 RUN pip install -r requirements.txt
 COPY app.py .
 EXPOSE 5000
+RUN apt-get update && apt-get install -y iproute2
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -5,23 +5,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-
+    # Get the computer's hostname and IP address
     computer_name = socket.gethostname()
-    computer_ip = socket.gethostbyname(computer_name)
     
-
+    try:
+        host_ip = subprocess.check_output(['hostname', '-I']).decode('utf-8').split()[0]
+    except:
+        host_ip = "Could not get host IP"
+    
+    # Create a simple HTML page
     html_page = f"""
     <html>
         <body>
             <h1>Welcome to my website!</h1>
             <h2>Server Information:</h2>
-            <p>Computer Name: {computer_name}</p>
-            <p>IP Address: {computer_ip}</p>
+            <p>Computer Name: {computer_nme}</p>
+            <p>IP Address: {compuer_atip}</p>
         </body>
     </html>
     """
     return html_page
 
-
+# Start the website
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
